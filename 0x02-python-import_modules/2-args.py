abfc3ba8f8c2c65860df10f03c43dabb790e4fd0
nasergamal <naser.gamal98@gmail.com>
2023-05-11 17:06:22 +0200

    2- typo for 1 argument printing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,9 @@ import sys
 if len(sys.argv) == 1:
     print("0 arguments.")
     exit()
-print("{} arguments:".format(len(sys.argv) - 1))
+elif len(sys.argv) == 2:
+    print("{} argument:".format(len(sys.argv) - 1))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
 for i in range(1, len(sys.argv)):
     print("{}: {}".format(i, sys.argv[i]))
