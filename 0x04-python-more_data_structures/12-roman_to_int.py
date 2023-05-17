c78330982856967f21f5dfc7834c4e63b2cb41e1
nasergamal <naser.gamal98@gmail.com>
2023-05-17 18:09:38 +0200

    3-4-return empty_list hahandling 10-change verification method 12-code change

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,56 +3,15 @@ def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return
     su = 0
+    vald = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
+        if roman_string[i] not in vald:
+            return 0
         if i == len(roman_string) - 1:
-            if roman_string[i] == 'I':
-                su += 1
-            elif roman_string[i] == 'V':
-                su += 5
-            elif roman_string[i] == 'X':
-                su += 10
-            elif roman_string[i] == 'L':
-                su += 50
-            elif roman_string[i] == 'C':
-                su += 100
-            elif roman_string[i] == 'D':
-                su += 500
-            return su
-        if roman_string[i] == 'I':
-            if roman_string[i + 1] == 'V':
-                su += 5
-                i += 1
-            elif roman_string[i + 1] == 'X':
-                su += 9
-                i += 1
+            su += vald[roman_string[i]]
+        else:
+            if vald[roman_string[i]] < vald[roman_string[i + 1]]:
+                su -= vald[roman_string[i]]
             else:
-                su += 1
-            continue
-        elif roman_string[i] == 'V':
-            su += 5
-        elif roman_string[i] == 'X':
-            if roman_string[i + 1] == 'L':
-                su += 40
-                i += 1
-            elif roman_string[i + 1] == 'C':
-                su += 90
-                i += 1
-            else:
-                su += 10
-            continue
-        elif roman_string[i] == 'L':
-            su += 50
-        elif roman_string[i] == 'C':
-            if roman_string[i + 1] == 'D':
-                su += 400
-                i += 1
-            elif roman_string[i + 1] == 'M':
-                su += 900
-                i += 1
-            else:
-                su += 100
-        elif roman_string[i] == 'D':
-            su += 500
-        elif roman_string[i] == 'M':
-            su += 1000
+                su += vald[roman_string[i]]
     return su
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return
-    return set(set_1 & set_2)
+    return set_1 & set_2
