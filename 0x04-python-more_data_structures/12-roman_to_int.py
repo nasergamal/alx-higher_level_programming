945e2caaed59847f852c84130b2c18ec12ff2a67
nasergamal <naser.gamal98@gmail.com>
2023-05-17 18:15:58 +0200

    12-return value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
-        return
+        return 0
     su = 0
     vald = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
