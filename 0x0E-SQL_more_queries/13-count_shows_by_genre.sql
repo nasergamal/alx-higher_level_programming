34299531bf9001e37b4f24ac603378dc2a09fd46
nasergamal <naser.gamal98@gmail.com>
2023-07-19 23:23:38 +0200

    13- added missing column name

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT `g`.`name`, COUNT(*) AS number_of_shows
+SELECT `g`.`name` AS `genre`, COUNT(*) AS `number_of_shows`
 FROM `tv_genres` AS g
 	INNER JOIN `tv_show_genres` AS sg
 	ON `sg`.`genre_id` = `g`.`id`
