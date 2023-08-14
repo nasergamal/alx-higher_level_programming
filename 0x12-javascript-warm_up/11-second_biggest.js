e4e4a72fe986f4e328d2bc5a4a3443d835a82c6a
nasergamal <naser.gamal98@gmail.com>
2023-08-15 02:35:37 +0300

    11- method change

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 
-let max = process.argv[2];
-let max2;
-if (process.argv.length < 4) {
+const nums = [];
+const args = process.argv.splice(2, process.argv.length);
+const len = args.length;
+if (len <= 1) {
   console.log(0);
 } else {
-  for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > max) {
-      max2 = max;
-      max = process.argv[i];
-    } else if (process.argv[i] < max && (!max2 || process.argv[i] > max2)) {
-      max2 = process.argv[i];
-    }
+  for (let i = 0; i < len; i++) {
+    nums[i] = args[i];
   }
-  console.log(max2);
+  nums.sort((a, b) => a - b);
+  console.log(nums[len - 2]);
 }
