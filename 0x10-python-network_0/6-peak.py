6af4016647a19c12a7fbeb22861d0fc74ab381ef
nasergamal <naser.gamal98@gmail.com>
2023-08-31 17:25:24 +0300

    6 removed unnecessary var

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,10 +6,10 @@ def find_peak(list_of_integers):
     """find peak"""
     if not list_of_integers:
         return None
-    start, h = 0, len(list_of_integers)
+    h = len(list_of_integers)
     if h < 3:
         return max(list_of_integers)
-    mid = h // 2
+    mid = int(h / 2)
     if (list_of_integers[mid] > list_of_integers[mid - 1]
             and list_of_integers[mid] > list_of_integers[mid + 1]):
         return list_of_integers[mid]
