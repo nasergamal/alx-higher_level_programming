1a039da2e36366eed0fd4f092ea27845c5f7db5e
nasergamal <naser.gamal98@gmail.com>
2023-09-26 21:05:14 +0300

    101-

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
-let data = [];
+let data;
+function fprint (i) {
+  request(data[i], function (error, response2, body2) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body2).name);
+      if (i < data.length - 1) {
+        fprint(i + 1);
+      }
+    }
+  });
+}
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     data = JSON.parse(body).characters;
-    fprint();
+    fprint(0);
   }
 });
-async function fprint () {
-  for (const i in data) {
-    request(data[i], function (error, response2, body2) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(JSON.parse(body2).name);
-      }
-    });
-  }
-}
